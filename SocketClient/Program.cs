f838e9a8664af73c883f017e3a8f92dda5ef5d24
Gusiha(NB) <tima.milushev@mail.ru>
2023-11-18 13:12:02 +0300

    feat : all works fine

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Channels;
+
+string message = "Hello from NoteBook";
+Byte[] buffer = Encoding.UTF8.GetBytes(message);
+
 
 int port = 3245;
-IPAddress IPAddress = IPAddress.Parse("192.168.0.102");
+IPAddress IPAddress = IPAddress.Parse("192.168.0.105");
 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-TcpClient client = new();
-client.Connect(IPAddress, port);
+socket.Connect(IPAddress, port);    
+if(socket.Connected)
+{
+    Console.WriteLine("Connected");
+}
+
+socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
 
