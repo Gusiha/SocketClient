5fa1907d049dad513b173c0a19b96c374e1f1f03
Gusiha <TUGARIN@TUGARIN>
2023-11-18 11:43:11 +0300

    Add project files.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+int port = 3245;
+IPAddress IPAddress = IPAddress.Parse("192.168.0.102");
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+TcpClient client = new();
+client.Connect(IPAddress, port);
+
